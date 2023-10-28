0b0fb939754dee4dfa03c9624c726c24539b58b9
HooBearB <nbeeson708891@gmail.com>
2023-10-28 02:52:20 -0500

    Sync tagging framework, MAJOR timing issues however

---
+++ mGRFXLib.py
@@ -0,0 +1,65 @@
+def loadFile(filepath):
+    try:
+        file = open(filepath)
+        lines = file.readlines()
+        data = []
+        iter = 0
+        for line in lines:
+            linecontent = readLine(line)
+            for item in linecontent:
+                item["id"] = iter
+                iter = iter + 1
+            data = data + linecontent
+        print(f"mGRFXVis: loadFile() loaded {filepath} properly")
+        return data
+    except:
+        print("mGRFXLib: loadFile() has encountered an error, returning none")
+        return None
+    
+def readTag(tag):
+    headers = {"m": "float", "s": "float", "x": "int", "y": "int", "f": "int", "l": "int", "h": "int", "q": "int"}
+    tagData = {}
+    char = 0
+    while char < len(tag):
+        if tag[char] in headers.keys():
+            header = tag[char]
+            iter = (char + 1)
+            while tag[iter] not in headers.keys():
+                iter = iter + 1
+                if iter >= len(tag) - 1:
+                    break
+            
+            tagData[header] = tag[char + 1:iter]
+            if headers[header] == "float":
+                tagData[header] = float(tagData[header])
+            if headers[header] == "int":
+                tagData[header] = int(tagData[header])
+        char = char + 1
+    if "m" in tagData.keys():
+        if "s" in tagData.keys():
+            tagData["t"] = (tagData["m"] * 60) + tagData["s"]
+        else:
+            tagData["t"] = tagData["m"] * 60
+    elif "s" in tagData.keys():
+        tagData["t"] = tagData["s"]
+    return tagData
+
+def readLine(line):
+    iter = 0
+    rawdata = []
+    while iter < len(line):
+        if line[iter] == "<":
+            end = iter + 1
+            while line[end] != "<" and end + 1 < len(line):
+                end = end + 1
+            rawdata.append(line[iter:end])
+            iter = end - 1
+        iter = iter + 1
+    data = []
+    for rawObject in rawdata:
+        object = readTag(rawObject[:rawObject.find(">") + 1])
+        object["content"] = rawObject[rawObject.find(">") + 1:]
+        data.append(object)
+    return data
+
+print(loadFile(r"C:\Users\nbees\Music\femtanyl\CHASER\MURDEREVERY1UKNOW.GRV"))
--- mGRFXVis.py
+++ mGRFXVis.py
@@ -1,11 +1,13 @@
 from pyray import *
 from tkinter import filedialog
+import os
+import random
 import time
 import math
 import eyed3
 eyed3.log.setLevel("ERROR")
 from playsound import playsound
-import platform
+import mGRFXLib as mgrfx
 version = "0.0.1"
 
 class display:
@@ -31,9 +33,11 @@ class layout:
 themes = {
     "default": [(153, 255, 255, 255), (51, 153, 204, 64), (51, 153, 204, 128), (51, 153, 204, 255)],
     "inverted": [(51, 153, 204, 255), (153, 255, 255, 64), (153, 255, 255, 128), (153, 255, 255, 255)],
+    "e-ink": [(175, 175, 180, 255), (65, 65, 70, 64), (65, 65, 70, 128), (65, 65, 70, 255)],
     "solar": [(255, 255, 255, 255), (0, 0, 0, 64), (0, 0, 0, 128), (0, 0, 0, 255)],
     "lunar": [(0, 0, 0, 255), (255, 255, 255, 64), (255, 255, 255, 128), (255, 255, 255, 255)],
-    "sunset": [(40, 20, 35, 255), (240, 230, 40, 64), (240, 230, 40, 128), (240, 230, 40, 255)],
+    "sunset": [(40, 20, 35, 255), (255, 180, 40, 128), (247, 200, 40, 200), (240, 230, 40, 255)],
+    "lambda": [(57, 39, 27, 255), (98, 255, 26, 255), (21, 62, 168, 255), (208, 75, 0, 255)],
     "musikcube": [(24, 24, 20, 255), (220, 82, 86, 128), (230, 220, 116, 128), (166, 226, 46, 255)]
 }
 
@@ -44,6 +48,7 @@ class user:
 
 class current:
     working = False
+    gotFile = False
     title = "None"
     artist = "None"
     album = "None"
@@ -52,6 +57,8 @@ class current:
     order = 0
     iter = 0
     hold = 0
+    pause = 0
+    id = 0
 
 def determineString():
     displayOrder = ["Title: " + current.title, "Artist: " + current.artist, "Album: " + current.album]
@@ -59,11 +66,13 @@ def determineString():
         current.iter = 0
         current.order = current.order + 1
 
-    if current.hold < 50 and current.iter == 0:
+    if current.hold < 150 and current.iter == 0:
         current.hold = current.hold + 1
     else:
         current.hold = 0
-        current.iter = current.iter + 0.1
+        if current.pause % 4 == 0:
+            current.iter = current.iter + 0.1
+        current.pause = current.pause + 1
 
     return displayOrder[current.order % 3][int(current.iter):int(current.iter) + 18]
 
@@ -71,42 +80,51 @@ def promptFile(windowTitle, files):
     filepath = filedialog.askopenfilename(title = windowTitle, filetypes = files)
     if filepath == "":
         filepath = None
-    print(f"GRFXVis: promptFile() returned with {filepath}")
+    print(f"mGRFXVis: promptFile() returned with {filepath}")
     return filepath
 
 def formatTime(time):
-    return f"{math.floor(time / 60)}:{math.ceil(time % 60):02d}"
+    return f"{math.floor(time / 60)}:{math.floor(time % 60):02d}"
 
-def changeTheme():
-    themeList = list(themes.keys())
-    select = themeList.index(user.theme)
-    while not is_key_down(257):
-        begin_drawing()
-        clear_background(display.unlit)
-        x = 50
-        y = 25
-        for option in themeList:
-            if option == user.theme:
-                draw_text(f"- {option}", x, y, 50, display.quarter)
-                draw_text(f"- {option}", x + 3, y, 50, display.half)
-                draw_text(f"- {option}", x + 6, y, 50, display.lit)
-            else:
-                draw_text(option, x, y, 50, display.half)
-            if y >= 180:
-                x = x + 300
-                y = -25
-            y = y + 50
-        end_drawing()
-        if is_key_down(265) and select - 1 >= 0:
-            select = select - 1
-        elif is_key_down(264) and select + 1 < len(themeList):
-            select = select + 1
-        user.theme = themeList[select]
+def setTheme(theme = None):
+    if theme == None:
         display.unlit = themes[user.theme][0]
         display.quarter = themes[user.theme][1]
         display.half = themes[user.theme][2]
         display.lit = themes[user.theme][3]
-        time.sleep(0.2)
+    else:
+        user.theme = theme
+
+def changeTheme(theme = None):
+    themeList = list(themes.keys())
+    select = themeList.index(user.theme)
+    if theme == None:
+        while not is_key_down(257):
+            begin_drawing()
+            clear_background(display.unlit)
+            x = 50
+            y = 25
+            for option in themeList:
+                if option == user.theme:
+                    draw_text(f"- {option}", x, y, 50, display.quarter)
+                    draw_text(f"- {option}", x + 3, y, 50, display.half)
+                    draw_text(f"- {option}", x + 6, y, 50, display.lit)
+                else:
+                    draw_text(option, x, y, 50, display.half)
+                if y >= 180:
+                    x = x + 300
+                    y = -25
+                y = y + 50
+            end_drawing()
+            if is_key_down(265) and select - 1 >= 0:
+                select = select - 1
+            elif is_key_down(264) and select + 1 < len(themeList):
+                select = select + 1
+            user.theme = themeList[select]
+            setTheme()
+            time.sleep(0.2)
+    else:
+        setTheme(theme)
 
 def loadSong(filepath):
     try:
@@ -120,10 +138,10 @@ def loadSong(filepath):
         current.hold = 0
         music = load_music_stream(filepath)
         play_music_stream(music)
-        print(f"GRFXVis: loadSong() loaded {current.title} by {current.artist} from {current.album} [{current.length}s]")
+        print(f"mGRFXVis: loadSong() loaded {current.title} by {current.artist} from {current.album} [{current.length}s]")
         return music, True
     except:
-        print(f"GRFXVis: loadSong() was not able to return music object")
+        print(f"mGRFXVis: loadSong() was not able to return music object")
         current.title = "None"
         current.artist = "None"
         current.album = "None"
@@ -134,14 +152,12 @@ def loadSong(filepath):
         current.hold = 0
         return None, False
 
-def loadFile(filepath):
-    file = open(filepath)
-    events = []
-    
-    return events
+def getRandom():
+    return random.choice(os.listdir("C:\\"))
 
 def tryFile():
-    music, current.working = loadSong(promptFile("Select a track", [("Audio files (.mp3, .wav)", ["*.mp3", "*.wav"]), ("All files", "*.*")]))
+    music, current.working = loadSong(promptFile("Select a track", [("Audio files (.mp3, .wav, .ogg, .flac)", ["*.mp3", "*.wav", "*.ogg", "*.flac"]), ("All files", "*.*")]))
+    current.gotFile = False
     return music
 
 def determineStatus():
@@ -153,12 +169,12 @@ def determineStatus():
 def printLayout():
     begin_drawing()
     clear_background(display.unlit)
-    draw_text(f"GRFXVis {version}", 10, 275, 20, display.lit)
+    draw_text(f"mGRFXVis {version}", 10, 275, 20, display.lit)
     fTime = formatTime(current.time)
     draw_text(fTime, layout.currentTime["x"], layout.currentTime["y"], layout.currentTime["size"], display.lit)
     draw_text("/", layout.divider["x"], layout.divider["y"], layout.divider["size"], display.lit)
     draw_text(formatTime(current.length), layout.fileTime["x"], layout.fileTime["y"], layout.fileTime["size"], display.lit)
-    draw_text(determineStatus(), layout.status["x"], layout.status["y"], layout.status["size"], display.lit)
+    draw_text(determineStatus(), layout.status["x"], layout.status["y"], layout.status["size"], display.half)
     draw_text(f"Vol {int(user.volume * 100)}", layout.volume["x"], layout.volume["y"], layout.volume["size"], display.lit)
     draw_text(determineString(), layout.mainString["x"], layout.mainString["y"], layout.mainString["size"], display.lit)
     end_drawing()
@@ -175,11 +191,14 @@ def checkPause(music):
 
 def checkSeek(music):
     if is_key_down(262):
-        if current.time + 5 < current.length:
-            seek_music_stream(music, current.time + 5)
+        if not is_key_down(340):
+            if current.time + 5 < current.length:
+                seek_music_stream(music, current.time + 5)
+            else:
+                current.time = 0
+                seek_music_stream(music, current.time)
         else:
-            current.time = 0
-            seek_music_stream(music, current.time)
+            seek_music_stream(music, 0)
         time.sleep(0.2)
     if is_key_down(263):
         if current.time - 5 >= 0:
@@ -196,18 +215,32 @@ def checkVolume(music):
         else:
             user.volume = 1
         set_music_volume(music, user.volume)
-        time.sleep(0.03)
+        time.sleep(0.02)
     if is_key_down(264):
         if user.volume - 0.01 >= 0:
             user.volume = user.volume - 0.01
         else:
             user.volume = 0
         set_music_volume(music, user.volume)
-        time.sleep(0.03)
+        time.sleep(0.02)
 
+def checkPosition(music, linedata):
+    time = get_music_time_played(music)
+    for object in linedata:
+        if object["t"] < time - 1:
+            linedata.remove(object)
+    if len(linedata) > 0:
+        if time >= linedata[0]["t"]:
+            if current.id == linedata[0]["id"]:
+                print(f"mGRFXVis: {round(linedata[0]['t'], 4)} at {round(time, 4)} {linedata[0]['content']}")
+                current.id = current.id + 1
+
+
+        
 init_audio_device()
-init_window(display.x, display.y, f"GRFXVis {version}")
-set_target_fps(60)
+init_window(display.x, display.y, f"mGRFXVis {version}")
+set_target_fps(120)
+setTheme()
 printLayout()
 music = tryFile()
 while not window_should_close():
@@ -218,12 +251,17 @@ while not window_should_close():
             current.time = get_music_time_played(music)
     if is_key_down(341) and is_key_down(79):
         if is_key_down(340):
-            file = loadFile(promptFile("Select a lyric file", [("GRFXVis lyric files (.GRV, .txt)", ["*.GRV", "*.txt"]), ("All files", "*.*")]))
-        music = tryFile()
+            filedata = mgrfx.loadFile(promptFile("Select a lyric file", [("mGRFXVis lyric files (.GRV, .txt)", ["*.GRV", "*.txt"]), ("All files", "*.*")]))
+            seek_music_stream(music, 0)
+            current.gotFile = True
+            current.id = 0
+        else:
+            music = tryFile()
     if is_key_down(84):
         changeTheme()
     checkPause(music)
     checkSeek(music)
     checkVolume(music)
-    time.sleep(0.01)
+    if current.gotFile:
+        checkPosition(music, filedata)
 close_window()
